146b3e969e07357a0aba55e4832eb25ea0e30e98
rebia <rebianne03@naver.com>
2024-07-03 22:20:01 +0900

    2024.07.03 vue3-viz-flows : front - init commit

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 #### 사용자 액션 플로우차트 시각화 연습 프로젝트
 (완성 할수 있을진 모르겠다만 ㅠㅠ)
 
+** 사용한 npm package : https://v2.baklava.tech/getting-started.html
+
 This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-main>
+      <v-container>
+          <div style="width: 90vw; height: 90vh">
+              <BaklavaEditor :view-model="baklava" />
+          </div>
+      </v-container>
+  </v-main>
 </template>
+
+<script setup>
+import { BaklavaEditor, useBaklava } from "@baklavajs/renderer-vue"
+import "@baklavajs/themes/dist/syrup-dark.css";
+
+// https://v2.baklava.tech/getting-started.html 기본세팅 참고
+const baklava = useBaklava()
+</script>
