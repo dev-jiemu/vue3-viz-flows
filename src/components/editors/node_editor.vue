fc698c6a0e48193df4a46957e56721d8c77dc03d
jiye.kim <jiye.kim@callgate.co.kr>
2024-08-27 16:10:49 +0900

    2024.08.27 node-editor : front

--- src/components/editors/node_editor.vue
+++ src/components/editors/node_editor.vue
@@ -1,31 +1,73 @@
 <template>
     <v-row>
-        <v-col cols="8" class="text-h5 text-left mt-2 ml-2">
-            <v-icon size="small" class="mr-2 mb-1">mdi-update</v-icon>
+        <v-col cols="8" class="text-h5 text-left">
+            <v-icon size="x-small" class="mr-1 mb-1">mdi-file-edit-outline</v-icon>
             EDIT SCENARIO
+            <v-chip class="ml-2" variant="outlined" density="compact" color="success">{{ editScnInfo.type }}</v-chip>
+            :
+            <v-chip variant="outlined" density="compact" color="blue">{{ editScnInfo.seqno }}</v-chip>
         </v-col>
-        <v-col></v-col>
+        <v-col class="text-right">
+            <v-btn
+                class="mr-3"
+                variant="outlined"
+                color="orange"
+                prepend-icon="mdi-update"
+                @click="updateScnInfo"
+            >
+                UPDATE
+            </v-btn>
+        </v-col>
     </v-row>
     <v-divider class="ma-5"/>
-    <v-card class="ma-3">
+    <v-card class="ma-3" v-for="info in editScnInfo.step_list">
         <v-card-text class="ma-1">
             <v-row>
                 <v-col cols="6">
-                    {{editScnInfo}}
+                    <div class="text-h6 text-left mb-2">
+                        <v-chip
+                            class="mr-1 mb-1"
+                            label
+                            color="purple"
+                        >{{ info.step_order }}
+                        </v-chip>
+                        {{ info.step_id }}
+                    </div>
+                    <template v-for="action in info.event_actions">
+                        <v-card class="ma-3 position-relative" variant="outlined">
+                            <v-btn
+                                class="position-absolute top-0 right-0"
+                                style="transform: translate(-30%, 30%);"
+                                variant="text"
+                                density="compact"
+                                icon="mdi-pencil"></v-btn>
+                            <v-card-text>
+                                <template v-for="(value, key) in action">
+                                    <v-row>
+                                        <v-col cols="4" class="text-right">{{ key }}</v-col>
+                                        <v-col class="text-left">{{ value }}</v-col>
+                                    </v-row>
+                                </template>
+                            </v-card-text>
+                        </v-card>
+                    </template>
                 </v-col>
                 <v-col cols="6" class="text-left">
                     <span class="text-h6">Preview</span>
                     <vue-json-pretty
+                        :data="info"
                         style="background-color: aliceblue;"
-                        class="mt-2 ml-2"
+                        class="ma-2"
                         showLine
                         showIcon
-                        :data="editScnInfo"
                     ></vue-json-pretty>
                 </v-col>
             </v-row>
         </v-card-text>
     </v-card>
+    <v-dialog v-model="editModel" max-width="600">
+        <!-- TODO: -->
+    </v-dialog>
 </template>
 <script setup>
 import VueJsonPretty from 'vue-json-pretty'
@@ -74,15 +116,18 @@ const editScnInfo = computed({
         return result
     },
     set(newValue) {
-        // editInfo 에만 반영
     }
 })
 
+const editModel = ref(false)
+
 const route = useRoute()
 
+// TODO: json object to string
+const updateScnInfo = () => {
+    if (confirm('update?')) {
 
-const updateInfo = () => {
-
+    }
 }
 
 onMounted(() => {
---
+++ src/components/editors/props/popup_step_action_edit.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <v-row class="mt-1">
+                <v-col cols="8"></v-col>
+                <v-col cols="4" class="text-right">
+                    <v-btn variant="text" icon="mdi-close" density="comfortable" @click="closePopup"/>
+                </v-col>
+            </v-row>
+        </v-card-title>
+        <v-card-text>
+            <!-- TODO -->
+        </v-card-text>
+    </v-card>
+</template>
+<script setup>
+import {ref} from "vue";
+
+const props = defineProps(['item'])
+const emits = defineEmits(['close', 'update'])
+
+const editAction = ref(props.item)
+
+const closePopup = () => {
+    emits('close')
+}
+
+const updateAction = () => {
+    if(confirm('update?')) {
+
+    }
+}
+
+</script>
